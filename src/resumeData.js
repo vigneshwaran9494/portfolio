dac4b4e3b6fffb64152ef52b95a94514e45eb863
vigneshwaranoptisol <vigneshwaran.n@optisolbusiness.com>
2020-03-13 15:15:35 +0530

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,156 @@
+let resumeData = {
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Vignesh Waran",
+  role: "Senior Software Enginer",
+  linkedinId: "vigneshwaran9494",
+  skypeid: "wcvignesh",
+  roleDescription:
+    "About 4+ years of experience in IT services in the roles of software programmer, have expertise in open source as well as commercial platforms, Offering excellent programming and analytic skills ",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/vigneshwaran1/",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "http://github.com/vigneshwaran9494",
+      className: "fa fa-github"
+    },
+    {
+      name: "twitter",
+      url: "https://twitter.com/ucnvignesh",
+      className: "fa fa-twitter"
+    }
+  ],
+  aboutme:
+    "currently i am working as a senior software engineer at Optisol business solutions, have 4+ experience in mobile application development field. have developed mobile apps in various streams and different frameworks, my skills are java , kotlin, React Native and Flutter",
+  address: "India",
+  website: "wcvignesh@gmail.com",
+  education: [
+    {
+      UniversityName: "Anna university",
+      specialization:
+        "Bachelor Of Engineering (Computer Science and Engineering)",
+      MonthOfPassing: "June",
+      YearOfPassing: "2015",
+      Achievements: "Some Achievements"
+    }
+  ],
+  work: [
+    {
+      CompanyName: "Elysium Technologies Pvt Ltd",
+      specialization: "Android Developer",
+      MonthOfLeaving: "July 2015 - July 2016",
+      YearOfLeaving: " ",
+      Achievements:
+        "Design and develop android applications with core functionalities and rest apis"
+    },
+    {
+      CompanyName: "Optisol Business Solutions Pvt Ltd",
+      specialization: "Software Engineer",
+      MonthOfLeaving: "July 2016 - July 2018",
+      YearOfLeaving: " ",
+      Achievements:
+        " Conceptualize, develop, and test native Android applications, including UI, core functionality, and third-party SDKS . Architect Android applications with MVP pattern and dependency injection using Dagger"
+    },
+    {
+      CompanyName: "Optisol Business Solutions Pvt Ltd",
+      specialization: "Lead Engineer",
+      MonthOfLeaving: "July 2018 - April 2019",
+      YearOfLeaving: " ",
+      Achievements:
+        "Build 10+ applications in android native and cross platform applications using react native"
+    },
+    {
+      CompanyName: "Optisol Business Solutions Pvt Ltd",
+      specialization: "Senior Lead Engineer",
+      MonthOfLeaving: "April 2019 - Present",
+      YearOfLeaving: " ",
+      Achievements:
+        "create and maintain the react native applications using cutting edge technologies and lead a react native team of 4 members "
+    }
+  ],
+  skillsDescription: "",
+  skills: [
+    {
+      skillname: "Java"
+    },
+    {
+      skillname: "Kotlin"
+    },
+    {
+      skillname: "React-Native"
+    },
+    {
+      skillname: "Flutter"
+    },
+    {
+      skillname: "Ionic"
+    }
+  ],
+  portfolio: [
+    {
+      name: "LitPic-SDK",
+      description: "React Native",
+      imgurl: "images/portfolio/origami.jpg"
+    },
+    {
+      name: "Swift Motion",
+      description: "React Native",
+      imgurl: "images/portfolio/origami.jpg"
+    },
+    {
+      name: "Beacon Emergency Dispatch",
+      description: "React Native",
+      imgurl: "images/portfolio/origami.jpg"
+    },
+    {
+      name: "Palmetto Solar",
+      description: "React Native",
+      imgurl: "images/portfolio/origami.jpg"
+    },
+    {
+      name: "Musation",
+      description: "Android",
+      imgurl: "images/portfolio/origami.jpg"
+    },
+    {
+      name: "My ALAZEN-HR",
+      description: "Android",
+      imgurl: "images/portfolio/origami.jpg"
+    },
+    {
+      name: "Skynet",
+      description: "Android",
+      imgurl: "images/portfolio/origami.jpg"
+    },
+    {
+      name: "Smilables",
+      description: "Android",
+      imgurl: "images/portfolio/origami.jpg"
+    },
+    {
+      name: "Realfire",
+      description: "Android",
+      imgurl: "images/portfolio/origami.jpg"
+    },
+    {
+      name: "Instance App",
+      description: "Android",
+      imgurl: "images/portfolio/origami.jpg"
+    }
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
+    }
+  ]
+};
+
+export default resumeData;
