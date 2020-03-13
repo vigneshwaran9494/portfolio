b04c9dd61b47d88a8524c597676d23cedac52bcd
vigneshwaranoptisol <vigneshwaran.n@optisolbusiness.com>
2020-03-13 15:17:13 +0530

    Updates

---
+++ precache-manifest.ef3bd83ce8e0ca98c8ef90893d2ae1ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "10a7365cccfce1d29750",
+    "url": "/portfolio/static/css/main.f1160f59.chunk.css"
+  },
+  {
+    "revision": "10a7365cccfce1d29750",
+    "url": "/portfolio/static/js/main.10a7365c.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "112b18fc6e13722054de798712c719ab",
+    "url": "/portfolio/index.html"
+  }
+];
